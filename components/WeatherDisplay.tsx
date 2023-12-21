b3e080c8da687f2f0664a013f8280993389533bc
DAL <d4lj1t@hotmail.com>
2023-12-21 12:15:50 +0000

    trying

--- components/WeatherDisplay.tsx
+++ components/WeatherDisplay.tsx
@@ -47,7 +47,7 @@ export default function WeatherDisplay (): React.ReactNode {
             </div>
             {city?.name !== '' && (
               <div>
-                {console.log('city', city?)}
+                {console.log('cityss  ', city?)}
                   {/*{`${city.name} - ${city.country}`}*/}
               </div>
             )}
